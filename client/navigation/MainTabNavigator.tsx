4cc6be42ee81b008fef62895da837c90ac2a9f4f
christiandals <51626207-christiandals@users.noreply.replit.com>
2026-01-06 05:50:14 +0000

    Restored to 'b3bab3c8a055f1a32f6a83ab96aa55ed3215e61d'
    Replit-Restored-To: b3bab3c8a055f1a32f6a83ab96aa55ed3215e61d

--- client/navigation/MainTabNavigator.tsx
+++ client/navigation/MainTabNavigator.tsx
@@ -49,7 +49,7 @@ export default function MainTabNavigator() {
           backgroundColor: "#1A1A1A",
           borderTopWidth: 0,
           elevation: 0,
-          paddingTop: 5,
+          paddingBottom: 5,
         },
         headerShown: false,
       }}
